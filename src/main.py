7585823acc0e0e61dfaace9e80adc0fc4ab757a6
zuzozuo <duraledsedlex.xx@gmail.com>
2022-01-10 23:40:36 +0100

    a bit clearer code

--- src/main.py
+++ src/main.py
@@ -8,33 +8,32 @@ WINDOW_OFFSET = 2
 # ---- CLASSES 
 
 class Snake:
-    def __init__(self, width, height, win_x, win_y):
-        self.width = width
-        self.height = height
+    def __init__(self, w, h, win_x, win_y):
+        self.width = w
+        self.height = h
         self.win_x = win_x
         self.win_y = win_y
-        self.current_dir = [random.choice([-1,1]), random.choice([-1,1])]
+        self.current_dir = [0, random.choice([-1,1])]
         self.score = 0
 
 
         # SNAKE WINDOW INIT
-        self.window = curses.newwin(height, width, win_y, win_x)
+        self.window = curses.newwin(h, w, win_y, win_x)
         self.window.clear()
         self.window.box()
         self.window.border(1)
 
         # SNAKE START RANDOM POSITIONS
 
-        self.x = random.randint(2, int(width/2))
-        self.y = random.randint(2, int(height/2))
+        self.x = random.randint(2, int(w/2)) # current x pos
+        self.y = random.randint(2, int(h/2)) # current y pos
 
         # SNAKE LIST
         self.tail = [[self.x, self.y]]
 
         # FOOD LIST
-        self.food = [[random.randint(self.win_x+2, self.width-2), random.randint(self.win_y, self.height-2)]]
+        self.food = []
 
-    
     def move(self, direction):
         # NEW HEAD COORDS
         self.x += direction[0] 
@@ -43,14 +42,16 @@ class Snake:
         self.current_dir = direction
     
     def spawn_food(self):
-        new_food = [random.randint(self.win_x+2, self.width-2), random.randint(self.win_y, self.height-2)]
+
+        x = random.randint(self.win_x + 2, self.width - 2)
+        y = random.randint(self.win_y, self.height - 2)
         
-        while ((new_food in self.tail) or (new_food in self.food)): 
-            new_food = [random.randint(self.win_x+2, self.width-2), random.randint(self.win_y, self.height-2)]
-            
-        
-        self.food.append(new_food)
-    
+        while (([x, y] in self.tail) or ([x, y] in self.food)): 
+            x = random.randint(self.win_x + 2, self.width - 2)
+            y = random.randint(self.win_y, self.height - 2)
+
+        self.food.append([x, y])
+
     # ---- COLLISIONS
     def border_collision(self):
 
@@ -69,13 +70,15 @@ class Snake:
     
     def snake_tail(self):
         return self.tail
-    #------ 
+
+    # ----- 
     def add_score(self):
         self.score +=1
     
     def get_score(self):
         return self.score
 
+    # ----- 
     def update(self):
 
         # FOOD COLLISION
@@ -100,8 +103,10 @@ class Snake:
 
         self.window.addstr(to_del[1], to_del[0], " ")        
         
-        self.window.addstr(self.tail[0][1], self.tail[0][0], "O")
+        # drawing head
+        self.window.addstr(self.tail[0][1], self.tail[0][0], "#") 
 
+        # drawing the rest of the body
         for i in range(1, len(self.tail)):
             self.window.addstr(self.tail[i][1], self.tail[i][0], "o")
         
@@ -145,6 +150,8 @@ class Game:
         key = 0 
         event = 0
 
+        snake.spawn_food()
+
         while key != 27 and not is_collision:
             key = self.screen.getch()
 
@@ -167,7 +174,7 @@ class Game:
 
             snake.update()            
 
-            self.screen.addstr(1, self.width -20, "Score: " + str(snake.get_score()))
+            self.screen.addstr(1, self.width - 20, "Score: " + str(snake.get_score()))
             self.screen.refresh()
             time.sleep(0.1)
 
@@ -187,7 +194,6 @@ def main(screen):
         test = Game(screen, screen_x , screen_y)
         test.print_screen()
         test.run()
-
 
     except ValueError:
         print("Your terminal is too small! Current size x: {} y:{}".format(screen_y, screen_x))
